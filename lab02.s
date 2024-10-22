9bff7b03e800a5d86870b7776f7c221917abba30
GitHub_spyroskitsakis <cs05256@uoi.gr>
2024-10-22 15:43:18 +0300

    Update lab02.s

--- lab02.s
+++ lab02.s
@@ -11,6 +11,17 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 prog:
 #-----------------------------
 # Write your code here!
+    beq a1,zero,ret0
+    slli s0 ,a1,2
+    add s0,s0,a0
+loop:
+    addi so,so,-4
+    lw t1,0(s0)
+    beq t1,a2,done 
+    bne s0,a1,loop
+ret0:
+    add s0,zero,zero
+    
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
